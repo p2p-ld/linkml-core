c7e9f0c7461c9cbbf6ec1e269787c596fe9b3764
Frank Dekervel <kervel@gmail.com>
2025-06-20 08:53:51 +0200

    the first schema is the primary schema

--- src/schemaview/src/schemaview.rs
+++ src/schemaview/src/schemaview.rs
@@ -5,19 +5,24 @@ use linkml_meta::SchemaDefinition;
 use crate::curie::curie2uri;
 
 pub struct SchemaView {
-    schema_definitions: HashMap<String, SchemaDefinition>
+    schema_definitions: HashMap<String, SchemaDefinition>,
+    primary_schema: Option<String>,
 }
 
 impl SchemaView {
     pub fn new() -> Self {
         SchemaView {
             schema_definitions: HashMap::new(),
+            primary_schema: None,
         }
     }
 
     pub fn add_schema(&mut self, schema: SchemaDefinition) -> Result<(), String> {
         let schema_uri = &schema.id;
         self.schema_definitions.insert(schema_uri.to_string(), schema);
+        if self.primary_schema.is_none() {
+            self.primary_schema = Some(schema_uri.to_string());
+        }
         Ok(())
     }
 
