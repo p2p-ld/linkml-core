41c7be5923bad30b56c1f91ff2b0ee7883914809
Frank Dekervel <kervel@gmail.com>
2025-06-23 21:48:33 +0200

    fix cargo test

--- src/main/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use pyo3::prelude::*;
-use runtime::runtime_module as inner_module;
-
-#[pymodule(name = "linkml_runtime")]
-fn linkml_runtime(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    inner_module(m)
-}
